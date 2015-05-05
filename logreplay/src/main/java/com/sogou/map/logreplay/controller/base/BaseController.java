23798d805c922f44e212c0733187d5bf29aa911c
zyang <yzwcroco@gmail.com>
2015-05-05 14:29:57 +0800

    用ExceptionHandler来处理LogReplayException

--- logreplay/src/main/java/com/sogou/map/logreplay/controller/base/BaseController.java
+++ logreplay/src/main/java/com/sogou/map/logreplay/controller/base/BaseController.java
@@ -1,16 +1,29 @@
 package com.sogou.map.logreplay.controller.base;
 
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * 在摆脱对mengine-core包的依赖后，
- * 为了与前端代码保持兼容，
- * 而统一用successResult方法输出json结果
- */
+import com.sogou.map.logreplay.exception.LogReplayException;
+
 public abstract class BaseController {
 	
+	/**
+	 * 在摆脱对mengine-core包的依赖后，
+	 * 为了与前端代码保持兼容，
+	 * 而统一用successResult方法输出json结果
+	 */
 	protected ModelMap successResult(Object response) {
 		return new ModelMap("code", 0).addAttribute("response", response);
 	}
-
+	
+	/**
+	 * 统一处理LogReplayException
+	 */
+	@ResponseBody
+	@ExceptionHandler(LogReplayException.class)
+	protected ModelMap handleLogReplayException(LogReplayException e) {
+		return new ModelMap("code", e.getErrorId()).addAttribute("errorMsg", e.getErrorMsg());
+	}
+	
 }
