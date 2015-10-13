ff5f615eca9127a49ed8fb08c8a442fd8f2ccb29
yangziwen <yangziwen@baidu.com>
2015-10-13 14:24:20 +0800

    修改下spring的配置

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -156,7 +156,7 @@ CREATE TABLE `image` (
   `creator_id` INT UNSIGNED NOT NULL,
   `checksum` VARCHAR (28) NOT NULL,
   `format` VARCHAR (10) NOT NULL,
-  `type` SMALLINT, NOT NULL,
+  `type` SMALLINT NOT NULL,
   `width` INT NOT NULL,
   `height` INT NOT NULL,
   `size` INT NOT NULL,
