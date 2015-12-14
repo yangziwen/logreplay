f087502824b2fdce170671623302842ec98ffa37
yangziwen <yangziwen@baidu.com>
2015-12-14 18:41:58 +0800

    监控功能后端接口开发

--- src/main/java/com/sogou/map/logreplay/controller/MonitorController.java
+++ src/main/java/com/sogou/map/logreplay/controller/MonitorController.java
@@ -1,13 +1,19 @@
 package com.sogou.map.logreplay.controller;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sogou.map.logreplay.controller.base.BaseController;
+import com.sogou.map.logreplay.util.MonitorUtil;
+import com.sogou.map.logreplay.util.MonitorUtil.Data;
 import com.sogou.map.logreplay.util.SystemInfoUtil;
 
 @Controller
@@ -33,6 +39,48 @@ public class MonitorController extends BaseController {
 				.addAttribute("serverIcon", 			SystemInfoUtil.getServerIcon(serverInfo))
 				.addAttribute("pid", 					SystemInfoUtil.getPID())
 		;
+		return successResult(result);
+	}
+	
+	/**
+	 * 获取内存监控数据
+	 * @param startTime	开始时间(毫秒时间戳)
+	 * @param endTime	结束时间(毫秒时间戳)
+	 * @param step		取样间隔(秒)
+	 */
+	@ResponseBody
+	@RequestMapping("/memoryData")
+	public ModelMap getMemoryData(
+			@RequestParam(defaultValue = "0") long startTime,
+			@RequestParam(defaultValue = "0") long endTime,
+			@RequestParam(defaultValue = "300") long step,
+			HttpSession session) {
+		
+		if(startTime == 0) {
+			startTime = System.currentTimeMillis() - TimeUnit.HOURS.toMillis(3);
+		}
+		
+		if(endTime == 0) {
+			endTime = System.currentTimeMillis();
+		}
+		
+		String application = MonitorUtil.getCurrentApplication(session.getServletContext());
+		
+		List<Data<Long, Double>> usedMemoryDataList = MonitorUtil.getDataList(application, "usedMemory", startTime, endTime, step);
+		
+		List<Data<Long, Double>> usedNonHeapMemoryDataList = MonitorUtil.getDataList(application, "usedNonHeapMemory", startTime, endTime, step);
+		
+		List<Data<Long, Double>> usedPhysicalMemoryDataList = MonitorUtil.getDataList(application, "usedPhysicalMemorySize", startTime, endTime, step);
+		
+		List<Data<Long, Double>> usedSwapSpaceDataList = MonitorUtil.getDataList(application, "usedSwapSpaceSize", startTime, endTime, step);
+		
+		ModelMap result = new ModelMap()
+				.addAttribute("usedMemoryDataList", usedMemoryDataList)
+				.addAttribute("usedNonHeapMemoryDataList", usedNonHeapMemoryDataList)
+				.addAttribute("usedPhysicalMemoryDataList", usedPhysicalMemoryDataList)
+				.addAttribute("usedSwapSpaceDataList", usedSwapSpaceDataList)
+		;
+		
 		return successResult(result);
 	}
 	
