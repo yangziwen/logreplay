d16be1fada9a161bd6817eccf34cfd06fa27ee4d
yangziwen <yangziwen@baidu.com>
2016-04-05 16:47:47 +0800

    添加分页插件

---
+++ src/main/java/com/sogou/map/logreplay/dao/base/PaginationInterceptor.java
@@ -0,0 +1,95 @@
+package com.sogou.map.logreplay.dao.base;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.ReflectorFactory;
+import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
+import org.apache.ibatis.reflection.factory.ObjectFactory;
+import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
+import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
+import org.apache.ibatis.session.RowBounds;
+
+
+@Intercepts({
+	@Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class})
+})
+public class PaginationInterceptor implements Interceptor {
+	
+	private static final ObjectFactory DEFAULT_OBJECT_FACTORY = new DefaultObjectFactory();
+	private static final ObjectWrapperFactory DEFAULT_OBJECT_WRAPPER_FACTORY = new DefaultObjectWrapperFactory();
+	private static final ReflectorFactory DEFAULT_REFLECTION_FACTORY = new DefaultReflectorFactory();
+
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+		MetaObject metaStatementHandler = MetaObject.forObject(
+				statementHandler, 
+				DEFAULT_OBJECT_FACTORY,
+				DEFAULT_OBJECT_WRAPPER_FACTORY, 
+				DEFAULT_REFLECTION_FACTORY);
+		
+		// 分离代理对象链(由于目标类可能被多个拦截器拦截，从而形成多次代理，通过下面的两次循环可以分离出最原始的的目标类)
+		while (metaStatementHandler.hasGetter("h")) {
+			Object object = metaStatementHandler.getValue("h");
+			metaStatementHandler = MetaObject.forObject(object, 
+					DEFAULT_OBJECT_FACTORY, 
+					DEFAULT_OBJECT_WRAPPER_FACTORY, 
+					DEFAULT_REFLECTION_FACTORY);
+		}
+		
+		// 分离最后一个代理对象的目标类
+		while (metaStatementHandler.hasGetter("target")) {
+			Object object = metaStatementHandler.getValue("target");
+			metaStatementHandler = MetaObject.forObject(object, 
+					DEFAULT_OBJECT_FACTORY, 
+					DEFAULT_OBJECT_WRAPPER_FACTORY, 
+					DEFAULT_REFLECTION_FACTORY);
+		}
+
+		// 如果有设置rowBounds，则将rowBounds的参数追加到sql最后，实现物理分页
+		RowBounds rowBounds = (RowBounds) metaStatementHandler.getValue("delegate.rowBounds");
+		if (rowBounds.getOffset() > 0 || rowBounds.getLimit() != RowBounds.NO_ROW_LIMIT) {
+			BoundSql boundSql = (BoundSql) metaStatementHandler.getValue("delegate.boundSql");
+			Object parameterObject = boundSql.getParameterObject();
+			if (parameterObject == null) {
+				throw new NullPointerException("parameterObject is null!");
+			} else {
+				String sql = boundSql.getSql();
+				// 重写sql
+				String pageSql = sql + " LIMIT " + rowBounds.getOffset() + "," + rowBounds.getLimit();
+				metaStatementHandler.setValue("delegate.boundSql.sql", pageSql);
+				// 采用物理分页后，就不需要mybatis的内存分页了，所以重置下面的两个参数
+				metaStatementHandler.setValue("delegate.rowBounds.offset", RowBounds.NO_ROW_OFFSET);
+				metaStatementHandler.setValue("delegate.rowBounds.limit", RowBounds.NO_ROW_LIMIT);
+			}
+		}
+		// 将执行权交给下一个拦截器
+		return invocation.proceed();
+	}
+
+	@Override
+	public Object plugin(Object target) {
+		// 当目标类是StatementHandler类型时，才包装目标类，否者直接返回目标本身,减少目标被代理的次数
+		if (target instanceof StatementHandler) {
+			return Plugin.wrap(target, this);
+		} else {
+			return target;
+		}
+	}
+
+	@Override
+	public void setProperties(Properties properties) {
+		// do nothing
+	}
+
+}
